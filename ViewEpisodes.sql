556892257f64364125e51e7bc740c42228f5a2b5
duaa-braik <duaa13braik@gmail.com>
2023-02-11 23:21:45 +0200

    8) Episodes details View

--- ViewEpisodes.sql
+++ ViewEpisodes.sql
@@ -1,20 +1,12 @@
 use DoctorWho
-declare @id as int = 0
-select E.Title, a.AuthorName, d.DoctorName, Enemies.EnemyName, Companions.CompanionName
-from tblAuthor a
+GO
+create view viewEpisodes as (
+select E.Title, author.AuthorName, doctor.DoctorName, dbo.fnCompanions(E.EpisodeId) as Companions, dbo.fnEnemies(E.EpisodeId) as Enemies
+from tblAuthor author
 inner join tblEpisode E
-on E.AuthorId = a.AuthorId
-inner join tblDoctor d
-on d.DoctorId = E.DoctorId
-inner join 
-	dbo.fnEnemies(E.EpisodeId) as Enemies
-on E.EpisodeId = Enemies.EpisodeId
-inner join 
-	(select c.CompanionName, ec.EpisodeId
-	from tblCompanion c
-	inner join tblEpisodeCompanion ec
-	on ec.CompanionId = c.CompanionId) as Companions
-on E.EpisodeId = Companions.EpisodeId
+on E.AuthorId = author.AuthorId
+inner join tblDoctor doctor
+on doctor.DoctorId = E.DoctorId
+)
 
---replace the 2 subqueries by the functions
-
+select * from viewEpisodes
